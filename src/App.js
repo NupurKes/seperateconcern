31b1f30c7286383949ac590ee655ba9039a559e0
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-24 10:39:24 -0500

    clock Element

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import "./App.css";
 import React from 'react';
 
 function App() {
-  const [color, setColor] = React.useState(JSON.parse(sessionStorage.getItem('color')) || "blue");
-  const [count, setCount] = React.useState(JSON.parse(localStorage.getItem('count')) || 0);
-  const [time, setTime] = React.useState(new Date().toLocaleTimeString());
+  const [color, setColor] = React.useState(
+    JSON.parse(sessionStorage.getItem('color')) || "blue");
+  const [count, setCount] = React.useState(
+    JSON.parse(localStorage.getItem('count')) || 0);
+ 
       console.log(color);
       console.log(count);
 
       React.useEffect(() => {
   sessionStorage.setItem('color', JSON.stringify(color));
   localStorage.setItem('count', JSON.stringify(count));
-  const interval = setInterval(() => setTime(new Date().toLocaleTimeString(), 1000));
-  return () => {clearInterval(interval);}
+  
   }, [color, count]);
 
 
@@ -20,7 +21,7 @@ function App() {
   {/* This component has two children that display a message and a clock.
   It also has a button that counts clicks and toggles style color. */}
   <DisplayMessage color={color} />
-  <Clock time={time} />
+  <Clock />
   <p>{count}</p>
   <button onClick={() => {setColor(toggle(color)); setCount(count+1)}}>
   Click me React</button></div>
@@ -44,8 +45,15 @@ function App() {
   return <h1 style={{color: props.color}}>Hello React World</h1>;
   }
 
-   function Clock(props) {
-     return <p>React Clock: {props.time} </p>
+   function Clock() {
+    const [time, setTime] = React.useState(
+      new Date().toLocaleTimeString());
+      React.useEffect(() => {
+      const interval = setInterval(() =>
+      setTime(new Date().toLocaleTimeString()), 2000);
+      return () => {clearInterval(interval);}
+      }, [time]);
+      return <p>React Clock: {time} </p>
      }
   
 
