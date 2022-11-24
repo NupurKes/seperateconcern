567a88d55a23c7a0f1ee2f5bd292c9b68a01fca3
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-24 12:35:38 -0500

    components seperated

--- src/App.js
+++ src/App.js
@@ -1,72 +1,8 @@
 import "./App.css";
-import React ,{ useState , useEffect } from 'react';
+import  Main from "./components/Main";
 
 function App() {
-  const [color, setColor] = useState(
-    JSON.parse(sessionStorage.getItem('color')) || "blue");
-  const [count, setCount] = useState(
-    JSON.parse(localStorage.getItem('count')) || 0);
- 
-      console.log(color);
-      console.log(count);
+return <Main />;
+}
 
-      useEffect(() => {
-  sessionStorage.setItem('color', JSON.stringify(color));
-  localStorage.setItem('count', JSON.stringify(count));
-  
-  }, [color, count]);
-
-
-  return (<div>
-  {/* This component has two children that display a message and a clock.
-  It also has a button that counts clicks and toggles style color. */}
-  <DisplayMessage color={color} />
-  <Clock />
-  <CounterDisplay count={count} />
-  <UserActions 
-  setColor={setColor} 
-  setCount={setCount} 
-  count={count} 
-  color={color}/>
-  </div>
-  );
-  }
-
-
-  
-
-  function CounterDisplay(props) {
-    return <p>{props.count}</p>;
-    }
-
-  function UserActions(props) {
-    const handleOnClick = () => {
-      props.setColor(toggle(props.color));
-      props.setCount(props.count+1)};
-    return <button onClick={handleOnClick}>
-    Change color React </button>;
-    }
-
-  function toggle(color){
-      return color === "blue" ? "red" :  color === "red" ? "green" : "blue";  
-  }
-
-  
-  //function with props
-  function DisplayMessage(props) {
-  return <h1 style={{color: props.color}}>Hello React World</h1>;
-  }
-
-  //self contained clock element
-   function Clock() {
-    const [time, setTime] = React.useState(
-      new Date().toLocaleTimeString());
-      useEffect(() => {
-      const interval = setInterval(() =>
-      setTime(new Date().toLocaleTimeString()), 2000);
-      return () => {clearInterval(interval);}
-      }, [time]);
-      return <p>React Clock: {time} </p>
-     }
-  
 export default App;
