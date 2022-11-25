eeac71ddc9e14cddfc2f1eb4daf92bc75da6988b
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-25 09:59:46 -0500

    Routing the pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import "./App.css";
-import  Main from "./components/Main";
+
+import Home from  "./pages/Home";
+import Contact from  "./pages/Contact";
+import About from  "./pages/About";
+
+import { Route , Routes } from "react-router-dom";
 
 function App() {
-return <Main />;
+return (
+    <div>
+
+<Routes>
+<Route path="/" element={<Home />} />
+<Route path="/about" element={<About />} />
+<Route path="/contact" element={<Contact />} />
+</Routes>
+</div>)
 }
 
 export default App;
