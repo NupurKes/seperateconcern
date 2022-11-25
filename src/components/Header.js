b3986949a484df2322cfc4e99f94cf07707da844
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-25 11:40:18 -0500

    use navigate

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-
+import HomeButton from './HomeButton';
 
 export default function Header() {
   return (
     <div>
+        <div>
+            <HomeButton />
+        </div>
+        <div>
         <Link to="/">Visit Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
+        </div>
     </div>
   )
 }
