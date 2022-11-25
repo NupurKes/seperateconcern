518dc457cc507d188e5c3a01a66a7f127b42a0c6
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-25 10:33:22 -0500

    react routing with links

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div>
-        <Link to="/">Home</Link>
+        <Link to="/">Visit Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
     </div>
