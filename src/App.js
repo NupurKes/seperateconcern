ebfed47149607541a4b7fa745bc926023a5cb358
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-25 10:34:13 -0500

    adding header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import "./App.css";
-
 import Home from  "./pages/Home";
 import Contact from  "./pages/Contact";
 import About from  "./pages/About";
-
 import { Route , Routes } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
 return (
 <div>
-
-    <Routes>
+    <Header />
+ <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
