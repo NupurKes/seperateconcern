b476e4503d4deca253637836888770f156bf6805
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-25 12:07:59 -0500

    navigation color changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,44 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-import HomeButton from './HomeButton';
+import React from "react";
+
+import HomeButton from "./HomeButton";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <div>
-        <div>
-            <HomeButton />
-        </div>
-        <div>
-        <Link to="/">Visit Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        </div>
+      <div>
+        <HomeButton />
+      </div>
+      <div>
+        <NavLink
+          style={({ isActive }) =>
+            isActive ? { color: "yellow" } : { color: "blue" }
+          }
+          to="/"
+        >
+          {" "}
+          Home
+        </NavLink>
+        <NavLink
+          style={({ isActive }) =>
+            isActive ? { color: "yellow" } : { color: "blue" }
+          }
+          to="/about"
+        >
+          {" "}
+          About
+        </NavLink>
+
+        <NavLink
+          style={({ isActive }) =>
+            isActive ? { color: "yellow" } : { color: "blue" }
+          }
+          to="/contact"
+        >
+          {" "}
+          Contact
+        </NavLink>
+      </div>
     </div>
-  )
+  );
 }
