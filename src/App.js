1db463f1e140c7ccff0eca5a84bc6de94a56c401
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-30 12:34:28 -0500

    did some formating

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ function App() {
 return (
 <div>  
  <Routes>
- <Route path="/" element={<MainLayout />} >
-    <Route index element={<Home />} />
-    <Route path="about" element={<About />} />
-    <Route path="contact" element={<Contact />} /> 
-</Route>
-<Route path="*" element={<p>Invalid URL</p>} />
-    </Routes>
+    <Route path="/" element={<MainLayout />} >
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} /> 
+    </Route>
+        <Route path="*" element={<p>Invalid URL</p>} />
+</Routes>
 </div>);
 }
 
